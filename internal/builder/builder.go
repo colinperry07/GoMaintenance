e164132f8c0f9a6532d99e53b6ed696efd3141c8
colinperry07 <colin.w.perry@proton.me>
2026-04-09 02:48:06 -0400

    feat: added mostly complete ANSI builder compatibility

--- internal/builder/builder.go
+++ internal/builder/builder.go
@@ -8,12 +8,75 @@ import (
 type Option func(*Style)
 
 type Style struct {
-	foreground string
+	foreground    string
+	background    string
+	bold          string
+	dim           string
+	italic        string
+	underline     string
+	blinking      string
+	reverse       string
+	hidden        string
+	strikethrough string
 }
 
 func WithForeground(color int) Option {
 	return func(s *Style) {
 		s.foreground = fmt.Sprintf("%d", color)
+	}
+}
+
+func WithBackground(color int) Option {
+	return func(s *Style) {
+		s.background = fmt.Sprintf("%d", color)
+	}
+}
+
+func WithBold() Option {
+	return func(s *Style) {
+		s.bold = "1m"
+	}
+}
+
+func WithDim() Option {
+	return func(s *Style) {
+		s.dim = "2m"
+	}
+}
+
+func WithItalic() Option {
+	return func(s *Style) {
+		s.italic = "3m"
+	}
+}
+
+func WithUnderline() Option {
+	return func(s *Style) {
+		s.underline = "4m"
+	}
+}
+
+func WithBlinking() Option {
+	return func(s *Style) {
+		s.blinking = "5m"
+	}
+}
+
+func WithReverse() Option {
+	return func(s *Style) {
+		s.reverse = "7m"
+	}
+}
+
+func WithHidden() Option {
+	return func(s *Style) {
+		s.hidden = "8m"
+	}
+}
+
+func WithStrikethrough() Option {
+	return func(s *Style) {
+		s.strikethrough = "9m"
 	}
 }
 
@@ -27,6 +90,42 @@ func Build(textToModify string, opts ...Option) string {
 
 	if s.foreground != "" {
 		codes = append(codes, s.foreground)
+	}
+
+	if s.background != "" {
+		codes = append(codes, s.background)
+	}
+
+	if s.bold != "" {
+		codes = append(codes, s.bold)
+	}
+
+	if s.dim != "" {
+		codes = append(codes, s.dim)
+	}
+
+	if s.italic != "" {
+		codes = append(codes, s.italic)
+	}
+
+	if s.underline != "" {
+		codes = append(codes, s.underline)
+	}
+
+	if s.blinking != "" {
+		codes = append(codes, s.blinking)
+	}
+
+	if s.reverse != "" {
+		codes = append(codes, s.reverse)
+	}
+
+	if s.hidden != "" {
+		codes = append(codes, s.hidden)
+	}
+
+	if s.strikethrough != "" {
+		codes = append(codes, s.strikethrough)
 	}
 
 	if len(codes) == 0 {
